efb20295e0d2687e488b0ab6e2f022fc585c1464
fern-gossow <139264117+fern-gossow@users.noreply.github.com>
2023-07-25 11:13:50 +1000

    add to sst.m
    Added recitification and evacuation to sst class

--- sst.m
+++ sst.m
@@ -6,7 +6,8 @@ declare type SSTab;
 
 declare attributes SSTab: Tab, Weight;
 
-// Initialisation and basic attributes
+// INITIALISATION
+
 intrinsic SST(T::Tbl, n::RngIntElt) -> SSTab
 {Create a semistandard tableau with designated weight}
     require Parent(T) eq TableauIntegerMonoid(): "Tableau must be over the positive integers";
@@ -46,6 +47,8 @@ intrinsic SST(skew::SeqEnum[RngIntElt], elts::SeqEnum[SeqEnum[RngIntElt]]) -> SS
     return SST(T, Weight(T));
 end intrinsic;
 
+// BASIC ATTRIBUTES
+
 intrinsic Print(T::SSTab)
 {Print T}
     printf"%o", T`Tab;
@@ -67,8 +70,80 @@ end intrinsic;
 
 intrinsic Shape(T::SSTab) -> SeqEnum[RngIntElt]
 {Return the shape of T}
-    return T`Shape;
+    return Shape(T`Tab);
 end intrinsic;
 
-// Actions on SST
+intrinsic SkewShape(T::SSTab) -> SeqEnum[RngIntElt]
+{Return the skew shape of T}
+    return SkewShape(T`Tab);
+end intrinsic;
+
+intrinsic IsSkew(T::SSTab) -> Bool
+{Return whether T is a skew tableau}
+    return IsSkew(T`Tab);
+end intrinsic;
+
+// ACTIONS
+
+intrinsic JeuDeTaquin(T::SSTab, i::RngIntElt, j::RngIntElt) -> SSTab, RngIntElt, RngIntElt
+{Perform jdt, and record the vacated cell}
+    shT := Shape(T`Tab);
+    R := JeuDeTaquin(T`Tab, i, j);
+    shR := Shape(R) cat [0];
+    // Row with difference between Shape(R) and Shape(T)
+    row := Index([shT[x] - shR[x] : x in [1 .. #shT]], 1);
+    return SST(R,T`Weight), row, shT[row];
+end intrinsic;
+
+intrinsic InverseJeuDeTaquin(T::SSTab, i::RngIntElt, j::RngIntElt) -> SSTab, RngIntElt, RngIntElt
+{Perform inverse jdt, and record the vacated cell}
+    skT := SkewShape(T`Tab) cat [0];
+    R := InverseJeuDeTaquin(T`Tab, i, j);
+    skR := SkewShape(R);
+    // Row with difference between SkewShape(R) and SkewShape(T)
+    row := Index([skR[x] - skT[x] : x in [1 .. #skR]], 1);
+    return SST(R,T`Weight), row, skR[row];
+end intrinsic;
+
+intrinsic Rectify(T::SSTab) -> SSTab, SeqEnum[RngIntElt], SeqEnum[RngIntElt]
+{Rectify T, and record the vacated rows and columns}
+    R := T;
+    vacrows := [];
+    vaccols := [];
+    while IsSkew(R) do
+        // Find largest possible cell to slide into
+        i := Max([x : x in [1..#SkewShape(R)] | SkewShape(R)[x] gt 0]);
+        j := SkewShape(R)[i];
+        R, r, c := JeuDeTaquin(R, i, j);
+        Append(~vacrows, r);
+        Append(~vaccols, c);
+    end while;
+    return R, vacrows, vaccols;
+end intrinsic;
+
+intrinsic InverseRectify(T::SSTab, rows::SeqEnum[RngIntElt], cols::SeqEnum[RngIntElt]) -> SSTab
+{Unrectify T along a path}
+    require #rows eq #cols: "Length of rows and columns must be the same";
+    R := T;
+    for j in [1..#rows] do
+        R := InverseJeuDeTaquin(R, rows[j], cols[j]);
+    end for;
+    return R;
+end intrinsic;
+
+intrinsic Evacuate(T::SSTab) -> SSTab
+{Perform usual evacuation for tableaux, and reversal for skew tableaux}
+    if not IsSkew(T) then
+        r := Shape(T)[1];
+        padding := Reverse([r - Shape(T)[i] : i in [1..#Shape(T)]]);
+        rows := [Eltseq(x) : x in Rows(T`Tab)];
+        // Calculate rows after rotating and reversing values
+        elts := Reverse([Reverse([T`Weight - x + 1 : x in row]) : row in rows]);
+        X := Rectify(SST(padding,elts,T`Weight));
+        return X;
+    else
+        X, vr, vc := Rectify(T);
+        return InverseRectify(Evacuate(X), Reverse(vr), Reverse(vc));
+    end if;
+end intrinsic;
 
--- sst.sig
+++ sst.sig
@@ -2,6 +2,7 @@
 T,SSTab,-,0
 A,SSTab,2,Tab,Weight
 S,SST,Create a semistandard tableau with designated weight,0,2,0,0,0,0,0,0,0,148,,0,0,300,,SSTab,-38,-38,-38,-38
+S,SST,Create a semistandard tableau with designated weight,0,1,0,0,0,0,0,0,0,300,,SSTab,-38,-38,-38,-38
 S,SST,Create a SST with prescribed weight,1,0,1,82,1,82,0,148,2,0,0,0,0,0,0,0,148,,0,0,82,,SSTab,-38,-38,-38,-38
 S,SST,Create a SST with weight = number of boxes,1,0,1,82,1,82,0,148,1,0,0,0,0,0,0,0,82,,SSTab,-38,-38,-38,-38
 S,SST,Create a SST with prescribed weight given its skew shape,2,0,1,82,0,148,1,1,82,1,82,0,148,3,0,0,0,0,0,0,0,148,,0,0,82,,0,0,82,,SSTab,-38,-38,-38,-38
@@ -10,3 +11,10 @@ S,Print,Print T,0,1,0,0,1,0,0,0,0,SSTab,,-38,-38,-38,-38,-38
 S,+,Compose two skew tableaux by joining them together,0,2,0,0,0,0,0,0,0,SSTab,,0,0,SSTab,,SSTab,-38,-38,-38,-38
 S,Weight,Return the weight of T,0,1,0,0,0,0,0,0,0,SSTab,,148,-38,-38,-38,-38
 S,Shape,Return the shape of T,0,1,0,0,0,0,0,0,0,SSTab,,82,-38,-38,-38,-38
+S,SkewShape,Return the skew shape of T,0,1,0,0,0,0,0,0,0,SSTab,,82,-38,-38,-38,-38
+S,IsSkew,Return whether T is a skew tableau,0,1,0,0,0,0,0,0,0,SSTab,,37,-38,-38,-38,-38
+S,JeuDeTaquin,"Perform jdt, and record the vacated cell",0,3,0,0,0,0,0,0,0,148,,0,0,148,,0,0,SSTab,,SSTab,148,148,-38,-38
+S,InverseJeuDeTaquin,"Perform inverse jdt, and record the vacated cell",0,3,0,0,0,0,0,0,0,148,,0,0,148,,0,0,SSTab,,SSTab,148,148,-38,-38
+S,Rectify,"Rectify T, and record the vacated rows and columns",0,1,0,0,0,0,0,0,0,SSTab,,SSTab,82,82,-38,-38
+S,InverseRectify,Unrectify T along a path,2,1,1,82,0,148,2,1,82,0,148,3,0,0,0,0,0,0,0,82,,0,0,82,,0,0,SSTab,,SSTab,-38,-38,-38,-38
+S,Evacuate,"Perform usual evacuation for tableaux, and reversal for skew tableaux",0,1,0,0,0,0,0,0,0,SSTab,,SSTab,-38,-38,-38,-38
