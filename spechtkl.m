6c4fc7e0d448b80228c543112adc2022f7fd9068
fern-gossow <139264117+fern-gossow@users.noreply.github.com>
2023-07-29 10:46:19 +1000

    Added representation for KL

--- spechtkl.m
+++ spechtkl.m
@@ -13,7 +13,7 @@ intrinsic SymmetricGroupData(n::RngIntElt) -> GrpPerm, GrpFPCox, Map
         k := hom<S -> W | [Inverse(CoxeterElement(W)), W.1]>;
     end if;
     h := hom<S -> W | x :-> Inverse(k(x))>;
-    return S,W,h;
+    return S, W, h;
 end intrinsic;
 
 intrinsic MuCoefficient(v::GrpFPCoxElt, w::GrpFPCoxElt) -> RngIntElt
@@ -22,14 +22,14 @@ intrinsic MuCoefficient(v::GrpFPCoxElt, w::GrpFPCoxElt) -> RngIntElt
     l := Length(w) - Length(v);
     if l mod 2 eq 0 then
         return 0;
-    else if l ge 0 then
+    elif l ge 0 then
         return Coefficient(KLPolynomial(v,w), Round((l-1)/2));
     else
         return Coefficient(KLPolynomial(w,v), Round((-l-1)/2));
     end if;
 end intrinsic;
 
-intrinsic MuCoefficientMatrix(elts::SeqEnum[GrpFPCox]) -> AlgMatElt
+intrinsic MuCoefficientMatrix(elts::SeqEnum[GrpFPCoxElt]) -> AlgMatElt
 {Return matrix of mu coefficients between Coxeter group elements}
     M := ZeroMatrix(Integers(), #elts, #elts);
     for i in [1..#elts] do
@@ -42,11 +42,11 @@ intrinsic MuCoefficientMatrix(elts::SeqEnum[GrpFPCox]) -> AlgMatElt
     return M;
 end intrinsic;
 
-intrinsic KLRepresentationMatrices(elts::SeqEnum[GrpFPCox]) -> SeqEnum[AlgMatElt]
+intrinsic KLRepresentationMatrices(elts::SeqEnum[GrpFPCoxElt]) -> SeqEnum[AlgMatElt]
 {Generator matrices of KL representation for an ordered list of Coxeter elements}
     W := Parent(elts[1]);
-    require &and[Parent(elt) eq W : elt in elts]: "All elements must be from the same group"
-    M := MuCoefficientTable(elts);
+    require &and[Parent(elt) eq W : elt in elts]: "All elements must be from the same group";
+    M := MuCoefficientMatrix(elts);
     // Initialise generator matrices
     S := [ZeroMatrix(Integers(), #elts, #elts) : k in [1..#Generators(W)]];
     for i in [1..#elts] do
@@ -70,4 +70,10 @@ intrinsic KLRepresentationMatrices(elts::SeqEnum[GrpFPCox]) -> SeqEnum[AlgMatElt
         end for;
     end for;
     return S;
+end intrinsic;
+
+intrinsic KLRepresentation(elts::SeqEnum[GrpFPCoxElt]) -> SeqEnum[AlgMatElt]
+{Representation given by the KL basis}
+    W := Parent(elts[1]);
+    return Representation(GModule(W, KLRepresentationMatrices(elts)));
 end intrinsic;
--- spechtkl.sig
+++ spechtkl.sig
@@ -1,3 +1,5 @@
 166,0
 S,SymmetricGroupData,"Return the symmetric group, it's Coxeter presentation, and a map between them",0,1,0,0,0,0,0,0,0,148,,224,390,175,-38,-38
-S,bean,Checks for bean,0,1,0,0,1,0,0,0,0,36,,-38,-38,-38,-38,-38
+S,MuCoefficient,Return the mu coefficient between Coxeter group elements,0,2,0,0,0,0,0,0,0,391,,0,0,391,,148,-38,-38,-38,-38
+S,MuCoefficientMatrix,Return matrix of mu coefficients between Coxeter group elements,1,0,1,82,0,391,1,0,0,0,0,0,0,0,82,,177,-38,-38,-38,-38
+S,KLRepresentationMatrices,Generator matrices of KL representation for an ordered list of Coxeter elements,1,0,1,82,0,391,1,0,0,0,0,0,0,0,82,,82,-38,-38,-38,-38
