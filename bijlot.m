4595eefe4841a92f9e0029d479b1fd9a6e9014de
fern-gossow <139264117+fern-gossow@users.noreply.github.com>
2023-08-06 17:30:23 +1000

    Created bijlot for lower-order term bijections.

---
+++ bijlot.m
@@ -0,0 +1,30 @@
+// Find and test bijections up to lower-order terms (LOB)
+// Currently don't test for coefficient, but can add this
+
+// Determine if M acts by l.o.t. on the standard basis, return the perm
+function IsLowerOrderBijection(M)
+    // Take columns, add buffer value
+    R := [Eltseq(r) cat [1] : r in Rows(M)];
+    nonzero := [Minimum([i : i in [1..#R+1] | r[i] ne 0]) : r in R];
+    if {x : x in nonzero} eq {1..#R} then
+        // Return the inverse permutation of nonzero entries
+        return [Index(nonzero, i) : i in [1..#R]];
+    else
+        // If no such bijection, return an empty list
+        return [];
+    end if;
+end function;
+
+// Determine if M acts by l.o.t. for any ordering of the standard basis
+function HasLowerOrderBijection(M)
+    for p in Permutations({1..NumberOfRows(M)}) do
+        P := PermutationMatrix(Integers(), p);
+        Q := PermutationMatrix(Integers(), [Index(p, i) : i in [1..#p]]);
+        bij := IsLowerOrderBijection(P * M * Q);
+        if #bij gt 0 then
+            return bij, p;
+        end if;
+    end for;
+    // If no bijections are found
+    return [], [];
+end function;
